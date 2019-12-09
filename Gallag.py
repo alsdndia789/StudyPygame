e3f0c396660b6e6996333dceb45ce30b61b04625
alsdndia789 <32069273+alsdndia789@users.noreply.github.com>
2019-12-09 19:03:02 +0900

    2019 - 12 - 09 "게임으로 배우는 파이썬 응용"
    한번에 3발까지 발사 가능하도록함.
    
    책에 나온 beam , bomb 객체가 처음부터 미리 그려놓고, 맵 밖으로 이동하여서 사라진것처럼 표현하는것이기때문에 틀을 유지해서 변형함.

--- Gallag.py
+++ Gallag.py
@@ -1,12 +1,13 @@
 import sys
 from random import randint
 import pygame
-from pygame.locals import Rect, QUIT,  KEYDOWN, KEYUP, K_LEFT, K_RIGHT, K_SPACE, K_ESCAPE
+from pygame.locals import Rect, QUIT, KEYDOWN, KEYUP, K_LEFT, K_RIGHT, K_SPACE, K_ESCAPE
 
 pygame.init()
 pygame.key.set_repeat(5, 5)
 SURFACE = pygame.display.set_mode((800, 600))
 FPSCLOCK = pygame.time.Clock()
+
 
 class Drawable:
     def __init__(self, rect, offset0, offset1):
@@ -33,19 +34,20 @@ class Ship(Drawable):
 
 
 class Beam(Drawable):
-    def __init__(self):
-        super().__init__(Rect(300, 0, 24, 24), 0, 24)
+    def __init__(self, num=0):
+        super().__init__(Rect(300, -50, 24, 24), 0, 24)
+        self.num = 1
 
 
 class Bomb(Drawable):
     def __init__(self):
-        super().__init__(Rect(300, -50, 24, 24), 48, 72)
+        super().__init__(Rect(300, 650, 24, 24), 48, 72)
         self.time = randint(5, 220)
 
 
 class Alien(Drawable):
     def __init__(self, rect, offset, score):
-        super().__init__(rect, offset, offset+24)
+        super().__init__(rect, offset, offset + 24)
         self.score = score
 
 
@@ -59,25 +61,30 @@ def main():
     game_over = False
     moving_left = True
     moving_down = False
+    shot = False
     move_interval = 20
     counter = 0
     score = 0
     aliens = []
     bombs = []
+    beams = []
     ship = Ship()
-    beam = Beam()
+    #beam = Beam()
     SIZE = 50
 
     ## alien 생성 ##
     for ypos in range(4):
         offset = 96 if ypos < 2 else 144
         for xpos in range(10):
-            rect = Rect(100 + xpos*SIZE, ypos*SIZE + 50, 24, 24)
-            alien = Alien(rect, offset, (4-ypos) * 10)
+            rect = Rect(100 + xpos * SIZE, ypos * SIZE + 50, 24, 24)
+            alien = Alien(rect, offset, (4 - ypos) * 10)
             aliens.append(alien)
 
     for _ in range(4):
         bombs.append(Bomb())
+
+    for _ in range(3):
+        beams.append(Beam())
 
     while True:
         ship_move_x = 0
@@ -86,21 +93,22 @@ def main():
                 pygame.quit()
                 sys.exit()
             elif event.type == KEYDOWN:
-                if event.key == K_LEFT:
-                    ship_move_x = -5
-                elif event.key == K_RIGHT:
-                    ship_move_x = +5
-                elif event.key == K_SPACE and beam.rect.bottom < 0:
-                    beam.rect.center = ship.rect.center
+                for beam in beams:
+                    if event.key == K_LEFT:
+                        ship_move_x = -5
+                    elif event.key == K_RIGHT:
+                        ship_move_x = +5
+                    elif event.key == K_SPACE and beam.rect.top > 600:
+                        shot = True
             if event.type == KEYUP:
                 if event.key == K_ESCAPE:
                     pygame.quit()
                     sys.exit()
 
         if not game_over:
+            counter += 1
             ship.move(ship_move_x, 0)
 
-            beam.move(0, -15)
 
             area = aliens[0].rect.copy()
             for alien in aliens:
@@ -126,7 +134,7 @@ def main():
 
             for bomb in bombs:
                 if bomb.time < counter and bomb.rect.top < 0:
-                    enemy = aliens[randint(0, len(aliens) -1)]
+                    enemy = aliens[randint(0, len(aliens) - 1)]
                     bomb.rect.center = enemy.rect.center
 
                 if bomb.rect.top > 0:
@@ -139,22 +147,34 @@ def main():
                 if bomb.rect.colliderect(ship.rect):
                     game_over = True
 
-                tmp = []
+            for beam in beams:
+                if shot and beam.num == 1:
+                    beam.rect.center = ship.rect.center
+                    beam.num = 0
+                    shot = False
+
+                if beam.rect.top < 600:
+                    beam.move(0, -15)
+
+                if beam.rect.top < 0:
+                    beam.rect.top = 650
+                    beam.num = 1
+
                 for alien in aliens:
                     if alien.rect.collidepoint(beam.rect.center):
-                        beam.rect.top = -50
+                        beam.rect.top = 650
                         score += alien.score
-                    else:
-                        tmp.append(alien)
-                aliens = tmp
-                if len(aliens) == 0:
-                    game_over = True
+                        aliens.remove(alien)
+                        beam.num = 1
+            if len(aliens) == 0:
+                game_over = True
 
         SURFACE.fill((0, 0, 0))
         for alien in aliens:
             alien.draw()
         ship.draw()
-        beam.draw()
+        for beam in beams:
+            beam.draw()
         for bomb in bombs:
             bomb.draw()
 
@@ -170,6 +190,7 @@ def main():
 
         pygame.display.update()
         FPSCLOCK.tick(20)
+
 
 if __name__ == '__main__':
     main()
